fab1ce8c69c3420043f80886559cd7b2107e3206
wushiwuwendong <wushiwuwendong@foxmail.com>
2023-06-10 17:37:01 +0800

    111

--- router/upload.js
+++ router/upload.js
@@ -14,19 +14,27 @@ const client = createClient(
 );
 
 const storage = multer.diskStorage({
-
+    //保存路径
+   /* destination: function (req, file, cb) {
+      cb(null, '/var/task/upload')
+      //注意这里的文件路径,不是相对路径，直接填写从项目根路径开始写就行了
+    },
+    //保存在 destination 中的文件名
+    filename: function (req, file, cb) {    
+      cb(null, file.originalname)
+    }*/
   })
 const upload = multer({ storage: storage })
 router.post("/upload",upload.single("image"),(req,res)=>{
     // 获取保存的图片信息
     try{
-      const { filename, size, path } = req.file;
+      const { originalname,filename, size, path } = req.file;
 
       // 构造响应数据
       const response = {
         status: 200,
         message: "收到图片",
-        filename: filename,
+        filename: originalname,
         size: size,
         path: path,
         list: client.getDirectoryContents("/ghost/hxj/upload")
