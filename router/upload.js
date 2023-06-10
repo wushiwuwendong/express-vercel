e7f51103560ce6764c98ff9f38b03871ff6e619f
wushiwuwendong <wushiwuwendong@foxmail.com>
2023-06-10 17:52:29 +0800

    11

--- router/upload.js
+++ router/upload.js
@@ -38,8 +38,8 @@ router.post("/upload",upload.single("image"),(req,res)=>{
         size: size,
         path: path,
         list: client.getDirectoryContents("/ghost/hxj/upload"),
-        uplaodstatus: client.putFileContents("/ghost/hxj/upload/"+originalname, fs.createReadStream(path)),
-        stream:fs.createReadStream(path)
+        uplaodstatus: client.putFileContents("/ghost/hxj/upload/"+originalname, fs.readFileSync(path)),
+        stream:fs.readFileSync(path)
       };
       res.json(response);
       // 将响应数据以 JSON 格式返回
