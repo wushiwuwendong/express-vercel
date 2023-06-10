e464d2ac9636b38de122131dbc113e198a27c3a7
wushiwuwendong <wushiwuwendong@foxmail.com>
2023-06-10 12:52:09 +0800

    111

--- router/upload.js
+++ router/upload.js
@@ -3,8 +3,19 @@ const router=EXPRESS.Router()
 const multer = require('multer')
 
 var fs = require('fs');//引用文件系统模块
-
-router.post("/upload",(req,res)=>{
+const upload = multer({ storage: storage })
+const storage = multer.diskStorage({
+    //保存路径
+    destination: function (req, file, cb) {
+      cb(null, '../upload')
+      //注意这里的文件路径,不是相对路径，直接填写从项目根路径开始写就行了
+    },
+    //保存在 destination 中的文件名
+    filename: function (req, file, cb) {    
+      cb(null, file.originalname)
+    }
+  })
+router.post("/upload",upload.single("image"),(req,res)=>{
     console.log(req.file)
     //console.log(typeof(req.file))
     res.json({status: "200", message: '收到图片'})
