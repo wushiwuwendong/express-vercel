d112390160447c0da3bb72968bae4b2651940bf4
jiealdong <50118661+wushiwuwendong@users.noreply.github.com>
2023-06-10 02:54:10 +0000

    111

--- router/download.js
+++ router/download.js
@@ -1,15 +1,22 @@
 const EXPRESS=require('express')
 const router=EXPRESS.Router()
-
-
-app.get("/download",(req, res)=>{
+var Queue = require('promise-queue')
+var maxConcurrent = 4;
+var maxQueue = Infinity;
+var queue = new Queue(maxConcurrent,maxQueue);
+router.get("/download",async function(req,res){
     //console.log(req.query['filename'])
-
-     
+    queue.add(function () {
+      fs.exists("../done/"+req.query['filename'],function(exists){
+        if(exists){
+            res.download("../done/"+req.query['filename'])
+            return
+        }else{
             res.json({status: "404", message: '文件不存在'})
-            
-  
-
+            return
+        }
+    })
+    })
    
     
     
