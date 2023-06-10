93d9d6fe83dd09ee75b70bff7efebbee15d23aa7
jiealdong <50118661+wushiwuwendong@users.noreply.github.com>
2023-06-10 03:04:31 +0000

    111

---
+++ router/upload.js
@@ -0,0 +1,23 @@
+const EXPRESS=require('express')
+const multer = require('multer')
+const router=EXPRESS.Router()
+const upload = multer({ storage: storage })
+var fs = require('fs');//引用文件系统模块
+const storage = multer.diskStorage({
+    //保存路径
+    destination: function (req, file, cb) {
+      cb(null, '../upload')
+      //注意这里的文件路径,不是相对路径，直接填写从项目根路径开始写就行了
+    },
+    //保存在 destination 中的文件名
+    filename: function (req, file, cb) {    
+      cb(null, file.originalname)
+    }
+  })
+router.post("../upload",upload.single("image"),(req,res)=>{
+    console.log(req.file)
+    console.log(typeof(req.file))
+    res.json({status: "200", message: '收到图片'})
+})
+
+module.exports=router
