16d05f8977af0e16deeadfe4539f785f80195f35
wushiwuwendong <wushiwuwendong@foxmail.com>
2023-06-10 12:45:02 +0800

    111

--- router/upload.js
+++ router/upload.js
@@ -4,7 +4,7 @@ const multer = require('multer')
 const upload = multer({ storage: storage })
 var fs = require('fs');//引用文件系统模块
 
-router.post("/upload",upload.single("image"),(req,res)=>{
+router.post("/upload",(req,res)=>{
     console.log(req.file)
     //console.log(typeof(req.file))
     res.json({status: "200", message: '收到图片'})
