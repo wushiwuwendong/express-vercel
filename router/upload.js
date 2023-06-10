6eedba4099bb08b68e31183a759450f8baa288d4
wushiwuwendong <wushiwuwendong@foxmail.com>
2023-06-10 12:46:17 +0800

    111

--- router/upload.js
+++ router/upload.js
@@ -1,7 +1,6 @@
 const EXPRESS=require('express')
 const router=EXPRESS.Router()
-const multer = require('multer')
-const upload = multer({ storage: storage })
+
 var fs = require('fs');//引用文件系统模块
 
 router.post("/upload",(req,res)=>{
