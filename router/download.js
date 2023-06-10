aacc39f93cd8839cb659a966cb710af911e01685
jiealdong <50118661+wushiwuwendong@users.noreply.github.com>
2023-06-10 02:47:26 +0000

    111

--- router/download.js
+++ router/download.js
@@ -1,9 +1,13 @@
 const EXPRESS=require('express')
 const router=EXPRESS.Router()
-
+var fs = require('fs');//引用文件系统模块
+var Queue = require('promise-queue')
+var maxConcurrent = 4;
+var maxQueue = Infinity;
+var queue = new Queue(maxConcurrent,maxQueue);
 app.get("/download",async function(req,res){
     //console.log(req.query['filename'])
-
+    queue.add(function () {
       fs.exists("../done/"+req.query['filename'],function(exists){
         if(exists){
             res.download("../done/"+req.query['filename'])
@@ -13,7 +17,7 @@ app.get("/download",async function(req,res){
             return
         }
     })
-    
+    })
    
     
     
