5ab63e23bf89579085e1c67f51b1749736e6e097
jiealdong <50118661+wushiwuwendong@users.noreply.github.com>
2023-06-10 02:33:05 +0000

    111

--- router/download.js
+++ router/download.js
@@ -1,5 +1,21 @@
 const EXPRESS=require('express')
 const router=EXPRESS.Router()
 
-
+app.get("/download",async function(req,res){
+    //console.log(req.query['filename'])
+    queue.add(function () {
+      fs.exists("../done/"+req.query['filename'],function(exists){
+        if(exists){
+            res.download("/root/jspdfcode/done/"+req.query['filename'])
+            return
+        }else{
+            res.json({status: "404", message: '文件不存在'})
+            return
+        }
+    })
+    })
+   
+    
+    
+})
 module.exports=router
