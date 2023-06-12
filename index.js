cfc1a89e7d4126edec1038817bad8814dbdf9aba
wushiwuwendong <wushiwuwendong@foxmail.com>
2023-06-12 10:57:47 +0800

    111

--- index.js
+++ index.js
@@ -23,7 +23,14 @@ app.use(EXPRESS.urlencoded({extended:false}))
 
 //静态文件托管
 // app.use('/static',EXPRESS.static("static"))
-
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By", ' 3.2.1');
+    //res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+  });
 //路由中间件
 app.use("/",listStoryR)
 app.use("/",listAnnR)
